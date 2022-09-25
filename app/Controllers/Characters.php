5fc05ecd62be6732fc38189c63b9c5c5310cec6a
Jeremy Putman <putman.jeremy@gmail.com>
2022-09-25 20:04:54 +0000

    Initial Setup

---
+++ app/Controllers/Characters.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Characters extends BaseController
+{
+    public function index()
+    {
+        echo 'Characters';
+    }
+    public function test(){
+        echo 'testing';
+    }
+}
